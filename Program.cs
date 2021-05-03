5c385f656dd07cae59d850cfe2f4ed6113b94e91
Elias Mjoen <Elias.Mjoen@students.fhnw.ch>
2021-05-04 01:29:16 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace XmlSort
+{
+    internal class Program
+    {
+        internal static void Sort(string path, string tagName, string content)
+        {
+            var desired = Directory.CreateDirectory(path+"/Desired");
+            var undesired = Directory.CreateDirectory(path + "/Undesired");
+            var files = Directory.GetFiles(path, "*.xml", SearchOption.TopDirectoryOnly);
+            foreach (var file in files)
+            {
+                var doc = XDocument.Load(file);
+                var tag = doc.Descendants(tagName).FirstOrDefault();
+                if (tag != null)
+                {
+                    if (tag.Value == content)
+                    {
+                        File.Move(file, desired.FullName + "/" + Path.GetFileName(file));
+                    }
+                    else
+                    {
+                        File.Move(file, undesired.FullName + "/" + Path.GetFileName(file));
+                    }
+                }
+            }
+        }
+        
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("#######################################################################################");
+            Console.WriteLine("Dieses Programm durchläufft einen Ordner voll XML Dateien.");
+            Console.WriteLine("In jeder Datei wird nach einem XML Tag gesucht.");
+            Console.WriteLine("Falls ein Tag gefunden wurde, wird der ERSTE genommen und auf INHALT überprüft:");
+            Console.WriteLine("Wenn der Tag den gewünschten Inhalt hat, wird er in den 'Desired' Ordner verschoben");
+            Console.WriteLine("Wenn der Tag den gewünschten Inhalt nicht hat, wird er in den Full Ordner verschoben");
+            Console.WriteLine("Falls der gesuchte Tag nicht gefunden wurde, wird die Datei übersprungen");
+            Console.WriteLine("#######################################################################################");
+
+            Console.WriteLine("Pfad zum Ordner mit den XML Dateien");
+            var path = Console.ReadLine();
+
+            Console.WriteLine("XML Tag nach dem gesucht werden soll");
+            var tagName = Console.ReadLine();
+
+            Console.WriteLine("Gewünschter Inhalt (Enter für 'leer')");
+            var content  = Console.ReadLine();
+            Sort(path, tagName, content);
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,18 @@
+﻿##################   DESCRIPTION   #########################################################
+
+This Program iterates over a specified directory ~ looking for xml files.
+It looks for a specified XML tag in every file. 
+If a tag is found, the first occurence in a file is checked for specified content:
+If the content of the xml tag matches the desired content, the file is moved to the ~/desired directory
+If the content of the xml tag doesnt match the desired content, the file is moved to the ~/undesired directory
+If the desired xml tag isnt found in a file, the file is skipped. 
+
+############################################################################################
+
+##################   EXECUTABLE   #########################################################
+
+The SELFCONTAINED executable is found in XMLSearch Folder. 
+It runs on Windows x86 Platform.
+A publishing Profile is created for x86 selfcontained. 
+
+############################################################################################
